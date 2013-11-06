47099260111121a624cb5e93f490a831c60b9fe6
Marc Dutoo <marc.dutoo@openwide.fr>
2013-11-06 17:02:50 +0100

    datacore deploy : added script & conf

---
+++ oasis-datacore-deploy/vmdc/home/oasis/install/tomcat7-dc/bin/setenv.sh
@@ -0,0 +1,10 @@
+## java
+export JAVA_HOME=/usr/lib/jvm/jdk1.7.0
+
+## production configuration :
+#export JAVA_OPTS="-Xms256m -Xmx512m -XX:MaxPermSize=128m"
+
+## debug :
+#JPDA_ADDRESS=8000
+#JPDA_SUSPEND=y
+
