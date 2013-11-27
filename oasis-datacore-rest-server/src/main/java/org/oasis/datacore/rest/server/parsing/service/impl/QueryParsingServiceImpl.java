89fac3ce9d064acc8b10e40734720192961be60a
spdy <ag.spdy@gmail.com>
2013-11-27 08:40:36 +0100

    #7 object mapper fix

--- oasis-datacore-rest-server/src/main/java/org/oasis/datacore/rest/server/parsing/service/impl/QueryParsingServiceImpl.java
+++ oasis-datacore-rest-server/src/main/java/org/oasis/datacore/rest/server/parsing/service/impl/QueryParsingServiceImpl.java
@@ -24,7 +24,7 @@ public class QueryParsingServiceImpl implements QueryParsingService {
 
 	@Autowired
 	@Qualifier("datacoreApiServer.objectMapper")
-	public ObjectMapper mapper = new ObjectMapper();
+	public ObjectMapper mapper;
 	
 	public void parseCriteriaFromQueryParameter(String fieldPath, String operatorAndValue, 
 												DCField dcField, DCQueryParsingContext queryParsingContext) 
