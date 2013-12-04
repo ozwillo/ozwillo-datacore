20d564b6698276528a088d8b8ed8ecf02c658e4d
Marc Dutoo <marc.dutoo@openwide.fr>
2013-12-04 18:52:29 +0100

    added fluent create test

--- oasis-datacore-rest-server/src/test/java/org/oasis/datacore/rest/server/DatacoreApiServerMixinTest.java
+++ oasis-datacore-rest-server/src/test/java/org/oasis/datacore/rest/server/DatacoreApiServerMixinTest.java
@@ -64,6 +64,15 @@ public class DatacoreApiServerMixinTest {
    @Autowired
    private DataModelServiceImpl modelAdminService; // TODO rm refactor
    
+   // .bdparcellaire
+   public static final String IGN_COMMUNE = "ign.commune";
+   public static final String IGN_DEPARTEMENT = "ign.departement";
+   public static final String IGN_CANTON = "ign.canton";
+   public static final String IGN_ARRONDISSEMENT = "ign.arrondissement";
+   public static final String IGN_DIVISION = "ign.division"; // Cadastrale
+   public static final String IGN_LOCALISANT = "ign.localisant"; // Parcellaire
+   public static final String IGN_PARCELLE = "ign.parcelle";
+   
    @Test // rather than @BeforeClass, else static and spring can't inject
    //@BeforeClass
    public /*static */void init1setupModels() {
@@ -73,7 +82,7 @@ public class DatacoreApiServerMixinTest {
       // field names are in French 
       // all fields are required
       
-      DCModel ignCommuneModel = new DCModel("ign.commune"); // rgc., pdp.
+      DCModel ignCommuneModel = new DCModel(IGN_COMMUNE); // rgc., pdp.
       /*ignCommuneModel.setDocumentation("{ \"uri\": \"http://localhost:8180/dc/type/country/France\", "
             + "\"name\": \"France\" }");*/
       ///ignCommuneModel.addField(new DCField("name", "string", true, 100)); // TODO cityCountry's ??!!
@@ -89,32 +98,32 @@ public class DatacoreApiServerMixinTest {
       ignCommuneModel.addField(new DCField("longitudeDMS", "int", true, 100)); // LONGI_DMS
       ignCommuneModel.addField(new DCField("latitudeDMS", "int", true, 100)); // LATI_DMS
 
-      DCModel ignDepartementModel = new DCModel("ign.departement"); // rgc., pdp.
+      DCModel ignDepartementModel = new DCModel(IGN_DEPARTEMENT); // rgc., pdp.
       ignDepartementModel.addField(new DCField("nom", "string", true, 100)); // DEP ; dep.nom ?
       ignDepartementModel.addField(new DCField("departementCode", "int", true, 100)); // DEP
 
-      DCModel ignCantonModel = new DCModel("ign.canton"); // rgc.
+      DCModel ignCantonModel = new DCModel(IGN_CANTON); // rgc.
       ignCantonModel.addField(new DCField("nom", "string", true, 100)); // ?? ; dep.nom ?
       ignCantonModel.addField(new DCField("departementCode", "int", true, 100)); // DEP
       ignCantonModel.addField(new DCField("arrondissementCode", "int", true, 100)); // ARRD
       ignCantonModel.addField(new DCField("cantonCode", "int", true, 100)); // CANT
 
-      DCModel ignArrondissementModel = new DCModel("ign.arrondissement"); // rgc., bdparcellaire.
+      DCModel ignArrondissementModel = new DCModel(IGN_ARRONDISSEMENT); // rgc., bdparcellaire.
       ignArrondissementModel.addField(new DCField("commune", "resource", true, 100));
       ignArrondissementModel.addField(new DCField("departementCode", "int", true, 100)); // DEP
       ignArrondissementModel.addField(new DCField("arrondissementCode", "int", true, 100)); // ARRD
       ignArrondissementModel.addField(new DCField("absorbeeCode", "int", true, 100)); // commune
       
-      DCModel ignDivisionModel = new DCModel("ign.division"); // bdparcellaire. ; cadastrale
+      DCModel ignDivisionModel = new DCModel(IGN_DIVISION); // bdparcellaire. ; cadastrale
       ignDivisionModel.addField(new DCField("sectionCadastrale", "string", true, 100)); // section
       ignDivisionModel.addField(new DCField("feuilleCadastrale", "int", true, 100)); // feuille
       
-      DCModel ignLocalisantModel = new DCModel("ign.localisant"); // bdparcellaire. ; parcellaire
+      DCModel ignLocalisantModel = new DCModel(IGN_LOCALISANT); // bdparcellaire. ; parcellaire
       ignLocalisantModel.addField(new DCField("sectionCadastrale", "string", true, 100)); // section
       ignLocalisantModel.addField(new DCField("feuilleCadastrale", "int", true, 100)); // feuille
       ignLocalisantModel.addField(new DCField("numeroParcelle", "int", true, 100)); // numero
 
-      DCModel ignParcelleModel = new DCModel("ign.parcelle"); // bdparcellaire.
+      DCModel ignParcelleModel = new DCModel(IGN_PARCELLE); // bdparcellaire.
       ignParcelleModel.addField(new DCField("sectionCadastrale", "string", true, 100)); // section
       ignParcelleModel.addField(new DCField("feuilleCadastrale", "int", true, 100)); // feuille
       ignLocalisantModel.addField(new DCField("numeroParcelle", "int", true, 100)); // numero
@@ -172,15 +181,44 @@ public class DatacoreApiServerMixinTest {
    }
    
    @Test
-   public void test() {
-      build("alt.tourism.placeKind", "hotel", new HashMap<String,Object>() {{
-         put("name", "hotel");
+   public void testBuild() {
+      String modelType = "alt.tourism.placeKind";
+      final String kind = "hotel";
+      @SuppressWarnings("serial")
+      DCResource r = build(modelType, kind, new HashMap<String,Object>() {{
+         put("name", kind);
       }});
-      DCResource.create(this.containerUrl, "alt.tourism.placeKind", "hotel").set("name", "hotel");
+      Assert.assertNotNull(r);
+      Assert.assertEquals(r.getUri(), this.containerUrl + "dc/type/" + modelType + '/' + kind);
+      Assert.assertEquals(r.getProperties().get("name"), kind);
+   }
+
+   @Test
+   public void testFluentCreate() {
+      String modelType = "alt.tourism.placeKind";
+      String kind = "winery";
+      DCResource r = DCResource.create(this.containerUrl, modelType, kind).set("name", kind);
+      Assert.assertNotNull(r);
+      Assert.assertEquals(r.getUri(), this.containerUrl + "dc/type/" + modelType + '/' + kind);
+      Assert.assertEquals(r.getProperties().get("name"), kind);
+   }
+
+   @Test
+   public void testFluentCreateService() {
+      String modelType = "alt.tourism.placeKind";
+      String kind = "monastery";
+      DCResource r = create(modelType, kind).set("name", kind);
+      Assert.assertNotNull(r);
+      Assert.assertEquals(r.getUri(), this.containerUrl + "dc/type/" + modelType + '/' + kind);
+      Assert.assertEquals(r.getProperties().get("name"), kind);
    }
    
    
    
+   private DCResource create(String modelType, String iri) {
+      return DCResource.create(this.containerUrl, modelType, iri);
+   }
+
    public DCResource buildAltTourismPlaceKind(String kind) {
       DCResource resource = new DCResource();
       String type = "alt.tourism.placeKind";
