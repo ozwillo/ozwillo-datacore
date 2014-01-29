a4a3d2f32f83520134c80c4d7c5aa72e1dc9e395
Marc Dutoo <marc.dutoo@openwide.fr>
2014-01-29 18:32:48 +0100

    removed useless import

--- oasis-datacore-rest-api/src/main/java/org/oasis/datacore/rest/api/DatacoreApi.java
+++ oasis-datacore-rest-api/src/main/java/org/oasis/datacore/rest/api/DatacoreApi.java
@@ -38,7 +38,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
 import com.wordnik.swagger.annotations.Api;
