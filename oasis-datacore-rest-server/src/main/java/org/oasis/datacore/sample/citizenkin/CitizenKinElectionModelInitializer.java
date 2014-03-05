14986e4a4a3169542f90df859ac2cca7c66b8944
Sylvain Chambon <sylvain.chambon@openwide.fr>
2014-03-05 15:30:48 +0100

    Update Citizen Kin electoral roll registration model (see pole-numerique/oasis-citizenkin#59)

--- oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/citizenkin/CitizenKinElectionModelInitializer.java
+++ oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/citizenkin/CitizenKinElectionModelInitializer.java
@@ -17,41 +17,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class CitizenKinElectionModelInitializer extends DatacoreSampleBase {
 
-
-    public static final String CITIZENKIN_PROCEDURE_ENVELOPE = "citizenkin.procedure.envelope";
     public static final String CITIZENKIN_PROCEDURE_ELECTORAL_ROLL_REGISTRATION = "citizenkin.procedure.electoral_roll_registration";
 
     public static final String STRING_TYPE = DCFieldTypeEnum.STRING.getType();
     public static final String DATE_TYPE = DCFieldTypeEnum.DATE.getType();
-    public static final String LONG_TYPE = DCFieldTypeEnum.LONG.getType();
-
 
     @Override
     public void doInit() {
 
-        DCModel envelope = new DCModel(CITIZENKIN_PROCEDURE_ENVELOPE);
-        envelope.addField(new DCField("definition_name", STRING_TYPE, true, 10));
-        envelope.addField(new DCField("initiator", STRING_TYPE, true, 100));
-        envelope.addField(new DCField("recipient", STRING_TYPE, false, 10));
-        envelope.addField(new DCField("state", STRING_TYPE, true, 10));
-        envelope.addField(new DCField("start_date", DATE_TYPE, true, 0));
-        envelope.addField(new DCField("modify_date", DATE_TYPE, false, 0));
-        envelope.addField(new DCField("comments", STRING_TYPE, false, 0));
-
-        modelAdminService.addModel(envelope);
-
-
         DCModel model = new DCModel(CITIZENKIN_PROCEDURE_ELECTORAL_ROLL_REGISTRATION);
 
-        model.addField(new DCResourceField("envelope", CITIZENKIN_PROCEDURE_ENVELOPE, true, 10));
-
-        model.addField(new DCField("nom_de_famille", STRING_TYPE, false, 0));
+        model.addField(new DCField("nom_de_famille", STRING_TYPE, true, 0));
         model.addField(new DCField("nom_d_usage", STRING_TYPE, false, 0));
-        model.addField(new DCField("prenom", STRING_TYPE, false, 0));
+        model.addField(new DCField("prenom", STRING_TYPE, true, 0));
         model.addField(new DCField("sexe", STRING_TYPE, true, 0));
-        model.addField(new DCField("date_de_naissance", DATE_TYPE, false, 0));
-        model.addField(new DCField("commune_de_naissance", STRING_TYPE, false, 0));
-        model.addField(new DCField("code_departement_de_naissance", STRING_TYPE, false, 0));
+        model.addField(new DCField("date_de_naissance", DATE_TYPE, true, 0));
+        model.addField(new DCField("commune_de_naissance", STRING_TYPE, true, 0));
+        model.addField(new DCField("code_departement_de_naissance", STRING_TYPE, true, 0));
         model.addField(new DCField("libelle_territoire_de_naissance_outremer", STRING_TYPE, false, 0));
         model.addField(new DCField("pays_de_naissance", STRING_TYPE, false, 0));
 
@@ -69,19 +51,18 @@ public class CitizenKinElectionModelInitializer extends DatacoreSampleBase {
         model.addField(new DCField("ambassade_poste_consulaire_origine", STRING_TYPE, false, 0));
         model.addField(new DCField("pays_origine", STRING_TYPE, false, 0));
 
-        model.addField(new DCField("adresse1", STRING_TYPE, false, 0));
+        model.addField(new DCField("adresse1", STRING_TYPE, true, 0));
         model.addField(new DCField("adresse2", STRING_TYPE, false, 0));
-        model.addField(new DCField("code_postal", STRING_TYPE, false, 0));
-        model.addField(new DCField("commune", STRING_TYPE, false, 0));
+        model.addField(new DCField("code_postal", STRING_TYPE, true, 0));
+        model.addField(new DCField("commune", STRING_TYPE, true, 0));
         model.addField(new DCField("telephone", STRING_TYPE, false, 0));
         model.addField(new DCField("courriel", STRING_TYPE, false, 0));
 
         /*
-         * in January 2014: points to a CK-local URL
-         * Later: points to a Digital Safe resource (somehow)
+         * in 2014: points to a CK-local URL
          */
-        model.addField(new DCField("justificatif_identite", STRING_TYPE, false, 0));
-        model.addField(new DCListField("justificatifs_domicile", new DCField("justificatif_domicile", STRING_TYPE, false, 0)));
+        model.addField(new DCField("justificatif_identite", STRING_TYPE, true, 0));
+        model.addField(new DCListField("justificatifs_domicile", new DCField("justificatif_domicile", STRING_TYPE, true, 0)));
 
 
         modelAdminService.addModel(model);
