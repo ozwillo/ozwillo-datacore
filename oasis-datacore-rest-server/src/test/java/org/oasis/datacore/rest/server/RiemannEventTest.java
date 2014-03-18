5e7fb1b37b688d104f8f2ca08e9ae23b25315817
Victor Voisin <victor.voisin@openwide.fr>
2014-03-18 15:27:15 +0100

    Added Riemann test with Riemann Java Client.

---
+++ oasis-datacore-rest-server/src/test/java/org/oasis/datacore/rest/server/RiemannEventTest.java
@@ -0,0 +1,61 @@
+package org.oasis.datacore.rest.server;
+
+import java.io.IOException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.aphyr.riemann.client.RiemannClient;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:oasis-datacore-rest-server-test-context.xml" })
+public class RiemannEventTest {
+	/*
+	 * WARNING: Riemann should be listening on port 5555
+	 * @Ignore
+	 */
+	@Test
+	@Ignore
+	public void sendSimpleEventToRiemann() {
+		RiemannClient c;
+		try {
+			c = RiemannClient.tcp("127.0.0.1", 5555);
+			c.connect();
+			c.event().
+			  service("queryLogging").
+			  state("running").
+			  description("?name=Lyon").
+			  metric(1).
+			  tags("query").
+			  ttl(30).
+			  send();
+			
+			c.event().
+			  service("queryLogging").
+			  state("running").
+			  description("?name=Lyon").
+			  metric(1).
+			  tags("query").
+			  ttl(30).
+			  send();
+			
+			c.event().
+			  service("queryLogging").
+			  state("running").
+			  description("?name=Paris").
+			  metric(1).
+			  tags("query").
+			  ttl(30).
+			  send();
+
+			//c.query("tagged \"cold\" and metric > 0"); // => List<Event>;
+			c.disconnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
