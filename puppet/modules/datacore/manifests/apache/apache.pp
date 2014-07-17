953a2fbf559deaa822171fd0e23a413cfab9278b
Victor Voisin <victor.voisin@openwide.fr>
2014-07-17 16:54:01 +0200

    Fixed Apache conf overwriting.

--- puppet/modules/datacore/manifests/apache/apache.pp
+++ puppet/modules/datacore/manifests/apache/apache.pp
@@ -1,17 +1,10 @@
 class datacore::apache::apache(
 
 ) inherits datacore::params {
-
-  file {'/etc/apache2/sites-available/default':
-    ensure => file,
-    #TODO!! : CHANGE!
-    source => 'puppet:///modules/datacore/oasis-datacore',
-  }
   
   exec {
     "Reload Apache":
-      command => "/etc/init.d/apache2 reload",
-      require => File['/etc/apache2/sites-available/default'];
+      command => "/etc/init.d/apache2 reload";
   }
 
 }
