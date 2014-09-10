bf54712492d9de10bb2a6e0c34b0d9532ebf81ec
Marc Dutoo <marc.dutoo@openwide.fr>
2014-09-10 19:52:52 +0200

    deploy : added envs conf, disabled ssl outside (pre)prod

--- puppet/modules/datacore/manifests/tomcat/tomcat.pp
+++ puppet/modules/datacore/manifests/tomcat/tomcat.pp
@@ -5,11 +5,13 @@ class datacore::tomcat::tomcat(
     exec {
       "Get Tomcat":
         command => "/usr/bin/wget ${tomcat_ftp} -O /home/oasis/upload/apache-tomcat-7.tar.gz",
-        timeout => 0;
+        timeout => 0,
+        unless  => "/bin/ls /home/oasis/upload | /bin/grep -c apache-tomcat-7";
       "Untar Tomcat":
         command => "/bin/tar xfz /home/oasis/upload/apache-tomcat-7.tar.gz -C /tmp",
         timeout => 0,
-        require => Exec['Get Tomcat'];
+        require => Exec['Get Tomcat'],
+        unless  => "/bin/ls /tmp | /bin/grep -c apache-tomcat-7";
       "Move Tomcat":
         command => "/bin/mv -f /tmp/apache-tomcat-7* /home/oasis/install/tomcat7-dc",
         timeout => 0,
