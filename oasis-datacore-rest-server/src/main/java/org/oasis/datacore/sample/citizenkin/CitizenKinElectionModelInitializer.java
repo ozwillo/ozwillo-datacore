a04869f3114f90fc7834379afae78fa6012c65c4
Sylvain Chambon <sylvain.chambon@openwide.fr>
2014-10-16 11:08:30 +0200

    Use prefixes for models and fields

--- oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/citizenkin/CitizenKinElectionModelInitializer.java
+++ oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/citizenkin/CitizenKinElectionModelInitializer.java
@@ -20,10 +20,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class CitizenKinElectionModelInitializer extends DatacoreSampleBase {
 
-    public static final String CITIZENKIN_PROCEDURE_ELECTORAL_ROLL_REGISTRATION = "citizenkin.procedure.electoral_roll_registration";
+    public static final String CITIZENKIN_PROCEDURE_ELECTORAL_ROLL_REGISTRATION = "citizenkin:electoral_roll_registration";
 
     public static final String STRING_TYPE = DCFieldTypeEnum.STRING.getType();
     public static final String DATE_TYPE = DCFieldTypeEnum.DATE.getType();
+    public static final String PREFIX = "ck_electoral_roll_registration:";
 
     @Override
     public void buildModels(List<DCModelBase> modelsToCreate) {
@@ -31,42 +32,42 @@ public class CitizenKinElectionModelInitializer extends DatacoreSampleBase {
         DCModel model = new DCModel(CITIZENKIN_PROCEDURE_ELECTORAL_ROLL_REGISTRATION);
         model.setDocumentation(""); // TODO
 
-        model.addField(new DCField("nom_de_famille", STRING_TYPE, true, 0));
-        model.addField(new DCField("nom_d_usage", STRING_TYPE, false, 0));
-        model.addField(new DCField("prenom", STRING_TYPE, true, 0));
-        model.addField(new DCField("sexe", STRING_TYPE, true, 0));
-        model.addField(new DCField("date_de_naissance", DATE_TYPE, true, 0));
-        model.addField(new DCField("commune_de_naissance", STRING_TYPE, true, 0));
-        model.addField(new DCField("code_departement_de_naissance", STRING_TYPE, true, 0));
-        model.addField(new DCField("libelle_territoire_de_naissance_outremer", STRING_TYPE, false, 0));
-        model.addField(new DCField("pays_de_naissance", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "nom_de_famille", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "nom_d_usage", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "prenom", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "sexe", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "date_de_naissance", DATE_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "commune_de_naissance", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "code_departement_de_naissance", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "libelle_territoire_de_naissance_outremer", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "pays_de_naissance", STRING_TYPE, false, 0));
 
         /* One of:
         "premiere_demande"
         "demenagement_meme_commune"
         "changement_commune"
          */
-        model.addField(new DCField("type_de_demande", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "type_de_demande", STRING_TYPE, true, 0));
 
-        model.addField(new DCField("commune_origine", STRING_TYPE, false, 0));
-        model.addField(new DCField("code_departement_origine", STRING_TYPE, false, 0));
-        model.addField(new DCField("libelle_territoire_outremer_origine", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "commune_origine", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "code_departement_origine", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "libelle_territoire_outremer_origine", STRING_TYPE, false, 0));
 
-        model.addField(new DCField("ambassade_poste_consulaire_origine", STRING_TYPE, false, 0));
-        model.addField(new DCField("pays_origine", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "ambassade_poste_consulaire_origine", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "pays_origine", STRING_TYPE, false, 0));
 
-        model.addField(new DCField("adresse1", STRING_TYPE, true, 0));
-        model.addField(new DCField("adresse2", STRING_TYPE, false, 0));
-        model.addField(new DCField("code_postal", STRING_TYPE, true, 0));
-        model.addField(new DCField("commune", STRING_TYPE, true, 0));
-        model.addField(new DCField("telephone", STRING_TYPE, false, 0));
-        model.addField(new DCField("courriel", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "adresse1", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "adresse2", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "code_postal", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "commune", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "telephone", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "courriel", STRING_TYPE, false, 0));
 
         /*
          * in 2014: points to a CK-local URL
          */
-        model.addField(new DCField("justificatif_identite", STRING_TYPE, true, 0));
-        model.addField(new DCListField("justificatifs_domicile", new DCField("justificatif_domicile", STRING_TYPE, true, 0)));
+        model.addField(new DCField(PREFIX + "justificatif_identite", STRING_TYPE, true, 0));
+        model.addField(new DCListField(PREFIX + "justificatifs_domicile", new DCField("justificatif_domicile", STRING_TYPE, true, 0)));
 
 
         modelsToCreate.addAll(Arrays.asList(new DCModelBase[] { model }));
--- oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/citizenkin/CitizenKinEnvironmentalIncidentsModelInitializer.java
+++ oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/citizenkin/CitizenKinEnvironmentalIncidentsModelInitializer.java
@@ -16,27 +16,28 @@ import java.util.List;
 @Component
 public class CitizenKinEnvironmentalIncidentsModelInitializer extends DatacoreSampleBase {
 
-    public static final String CITIZENKIN_PROCEDURE_ENVIRONMENTAL_INCIDENTS_DECLARATION = "citizenkin.procedure.environmental-incidents";
+    public static final String CITIZENKIN_PROCEDURE_ENVIRONMENTAL_INCIDENTS_DECLARATION = "citizenkin:environmental-incidents";
 
     public static final String STRING_TYPE = DCFieldTypeEnum.STRING.getType();
     public static final String DATE_TYPE = DCFieldTypeEnum.DATE.getType();
+    public static final String PREFIX = "ck_environmental-incidents:";
 
 
     @Override
     protected void buildModels(List<DCModelBase> models) {
         DCModel model = new DCModel(CITIZENKIN_PROCEDURE_ENVIRONMENTAL_INCIDENTS_DECLARATION);
         model.setDocumentation(""); // TODO: provide documentation for that model
-        model.addField(new DCField("nom", STRING_TYPE, true, 0));
-        model.addField(new DCField("cognom", STRING_TYPE, true, 0));
-        model.addField(new DCField("telefon", STRING_TYPE, false, 0));
-        model.addField(new DCField("email", STRING_TYPE, false, 0));
-        model.addField(new DCField("municipiusuari", STRING_TYPE, false, 0));
-        model.addField(new DCField("tipususuari", STRING_TYPE, true, 0));
-        model.addField(new DCField("data", DATE_TYPE, true, 0));
-        model.addField(new DCField("municipiincidencia", STRING_TYPE, true, 0));
-        model.addField(new DCField("tipusincidencia", STRING_TYPE, true, 0));
-        model.addField(new DCField("textincidencia", STRING_TYPE, false, 0));
-        model.addField(new DCField("adjuntarimatge", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "nom", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "cognom", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "telefon", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "email", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "municipiusuari", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "tipususuari", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "data", DATE_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "municipiincidencia", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "tipusincidencia", STRING_TYPE, true, 0));
+        model.addField(new DCField(PREFIX + "textincidencia", STRING_TYPE, false, 0));
+        model.addField(new DCField(PREFIX + "adjuntarimatge", STRING_TYPE, false, 0));
 
         models.add(model);
     }
