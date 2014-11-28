dfda3cbfb1a05a2d4620797028f53b5793a5b29c
Marc Dutoo <marc.dutoo@openwide.fr>
2014-11-28 18:29:07 +0100

    moved ResourceModelIniter.xxToResource() to ModelResourceMappingService

--- oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/ResourceModelIniter.java
+++ oasis-datacore-rest-server/src/main/java/org/oasis/datacore/sample/ResourceModelIniter.java
@@ -1,16 +1,12 @@
 package org.oasis.datacore.sample;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.oasis.datacore.core.meta.model.DCField;
 import org.oasis.datacore.core.meta.model.DCListField;
@@ -26,14 +22,10 @@ import org.oasis.datacore.model.event.ModelDCListener;
 import org.oasis.datacore.model.event.ModelResourceDCListener;
 import org.oasis.datacore.model.resource.ModelResourceMappingService;
 import org.oasis.datacore.rest.api.DCResource;
-import org.oasis.datacore.rest.api.util.DCURI;
-import org.oasis.datacore.rest.api.util.UriHelper;
 import org.oasis.datacore.rest.server.parsing.exception.ResourceParsingException;
 import org.oasis.datacore.rest.server.resource.ValueParsingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.google.common.collect.ImmutableList;
 
 
 /**
@@ -274,7 +266,7 @@ public class ResourceModelIniter extends DatacoreSampleBase {
    
    private void projectToResource(DCProject project, List<DCResource> resourcesToPost) {
       try {
-         final DCResource projectResource = projectToResource(project);
+         final DCResource projectResource = mrMappingService.projectToResource(project);
          resourcesToPost.add(projectResource);
       } catch (ResourceParsingException e) {
          logger.error("Conversion error building Resource from DCProject " + project, e);
@@ -283,32 +275,6 @@ public class ResourceModelIniter extends DatacoreSampleBase {
          logger.error("Unkown error building Resource from DCProject " + project, t);
          return; // TODO report errors ex. in list & abort once all are handled ?
       }
-   }
-   /** TODO move to ModelResourceMappingService */
-   public DCResource projectToResource(DCProject project) throws ResourceParsingException {
-      DCResource projectResource = DCResource.create(null, MODEL_PROJECT_NAME)
-            .set("dcmpv:name", project.getName())
-         
-            // NB. minor version is Resource version
-            .set("dcmpv:documentation", project.getDocumentation())// TODO in another collection for performance
-         
-            // TODO security
-            
-            // POLY cache : TODO
-            ;
-
-      //projectResource.set("dcmpv:pointOfViews", pointOfViewsToNames(project.getLocalVisibleProjects())); // TODO as cache ?!
-      projectResource.set("dcmp:localVisibleProjects", project.getLocalVisibleProjects().stream()
-            .map(p -> mrMappingService.buildNameUri(MODEL_PROJECT_NAME, p.getName())).collect(Collectors.toList())); // NOT toSet else JSON'd as HashMap
-      projectResource.set("dcmp:useCasePointOfViews", project.getUseCasePointOfViews().stream()
-            .map(ucpov -> {
-               //useCasePointOfViewToResource(ucpov); // TODO TODO & useCasePointOfViewElementToResource(ucpovelt)
-               //ucpov.getPointOfViews().stream().map(ucpovelt -> mrMappingService.buildPointOfViewUri(ucpovelt)).collect(Collectors.toList())); // NOT toSet else JSON'd as HashMap
-               return mrMappingService.buildNameUri(MODEL_POINTOFVIEW_NAME, ucpov.getName());
-            }).collect(Collectors.toList())); // NOT toSet else JSON'd as HashMap
-
-      projectResource.setUri(mrMappingService.buildPointOfViewUri(projectResource));
-      return projectResource;
    }
    
    private void modelsToResources(Collection<DCModelBase> models, List<DCResource> resourcesToPost) {
@@ -319,8 +285,8 @@ public class ResourceModelIniter extends DatacoreSampleBase {
    private void modelToResource(DCModelBase model, List<DCResource> resourcesToPost) {
       try {
          // filling model's provided props :
-         final DCResource modelResource = modelToResource(model);
-         modelFieldsAndMixinsToResource(model, modelResource);
+         final DCResource modelResource = mrMappingService.modelToResource(model);
+         mrMappingService.modelFieldsAndMixinsToResource(model, modelResource);
          
          // once props are complete, schedule post :
          resourcesToPost.add(modelResource);
@@ -331,230 +297,6 @@ public class ResourceModelIniter extends DatacoreSampleBase {
          logger.error("Unkown error building Resource from DCModel " + model, t);
          return; // TODO report errors ex. in list & abort once all are handled ?
       }
-   }
-   
-   /** TODO move to ModelResourceMappingService */
-   public DCResource modelToResource(DCModelBase model) throws ResourceParsingException {
-      DCModelBase definitionModel = modelAdminService.getDefinitionModel(model.getName());
-      DCModelBase storageModel = modelAdminService.getStorageModel(model.getName());
-      
-      // filling model's provided props :
-      DCResource modelResource = DCResource.create(null, MODEL_MODEL_NAME)
-            .set("dcmo:name", model.getName())
-            .set("dcmo:pointOfViewAbsoluteName", model.getPointOfViewAbsoluteName())
-            .set("dcmo:majorVersion", model.getMajorVersion())
-            
-             // POLY
-            .set("dcmo:isDefinition", model.isDefinition()) // = !dcmo:isStorageOnly
-            .set("dcmo:isStorage", model.isStorage())
-            .set("dcmo:isInstanciable", model.isInstanciable())
-         
-            // NB. minor version is Resource version
-            .set("dcmo:documentation", model.getDocumentation())// TODO in another collection for performance
-            
-            // storage :
-            ///.set("dcmo:collectionName", model.getCollectionName()) // POLY RATHER storageModel.getName()
-            .set("dcmo:maxScan", model.getMaxScan())
-            
-            // TODO security
-            
-            // instanciable :
-            .set("dcmo:isHistorizable", model.isHistorizable())
-            .set("dcmo:isContributable", model.isContributable())
-            
-            // POLY cache :
-            .set("dcmo:definitionModel", definitionModel.getName())
-            ;
-      if (storageModel != null) {
-         modelResource.set("dcmo:storageModel", storageModel.getName());
-      }
-      //modelResource.setVersion(model.getVersion()); // not at creation (or < 0),
-      // rather update DCModel.version from its resource after each put
-
-      // once id source props are complete, build URI out of them :
-      String uri = mrMappingService.buildModelUri(modelResource);
-      modelResource.setUri(uri);
-      return modelResource;
-   }
-
-   /** TODO move to ModelResourceMappingService */
-   public void modelFieldsAndMixinsToResource(DCModelBase model, DCResource modelResource) throws ResourceParsingException {
-      // still fill other props, including uri-depending ones :
-      DCURI dcUri;
-      try {
-         dcUri = UriHelper.parseUri(modelResource.getUri());
-      } catch (MalformedURLException | URISyntaxException e) {
-         throw new RuntimeException(e);
-      }
-      String fieldUriPrefix = mrMappingService.buildFieldUriPrefix(dcUri);
-      // allow to enrich existing fields' props :
-      @SuppressWarnings("unchecked")
-      List<Map<String, Object>> existingFieldsProps = (List<Map<String, Object>>) modelResource.get("dcmo:fields");
-      modelResource.set("dcmo:fields", fieldsToProps(model.getFieldMap(),
-            fieldUriPrefix, existingFieldsProps));
-      
-      ImmutableList.Builder<Object> mixinsPropBuilder = DCResource.listBuilder();
-      for (DCModelBase mixin : model.getMixins()) {
-         mixinsPropBuilder.add(mixin.getName());
-      }
-      modelResource.set("dcmo:mixins", mixinsPropBuilder.build());
-      
-      // fieldsAndMixinNames (to allow to rebuild them in DCModel in the right order,
-      // and for introspection) :
-      ImmutableList.Builder<Object> fieldAndMixinNamesBuilder = DCResource.listBuilder();
-      for (Object fieldOrMixin : model.getFieldAndMixins()) {
-         if (fieldOrMixin instanceof DCModelBase) { // TODO LATER common DCNamedBase with getName() ?
-            fieldAndMixinNamesBuilder.add(((DCModelBase) fieldOrMixin).getName());
-         } else { // assuming DCField
-            fieldAndMixinNamesBuilder.add(((DCField) fieldOrMixin).getName());
-         }
-      }
-      modelResource.set("dcmo:fieldAndMixins", fieldAndMixinNamesBuilder.build());
-      
-      // caches (so that they can be queried for introspection as well) :
-      modelResource.set("dcmo:globalMixins",
-            new ArrayList<String>(model.getGlobalMixinNames())); // TODO order
-      modelResource.set("dcmo:globalFields", fieldsToProps(model.getGlobalFieldMap(),
-            fieldUriPrefix, null)); // computed, so don't enrich existing props
-      
-      // filling company's resource props and missing referencing Mixin props : 
-      // - by building URI from known id/iri if no missing referencing Mixin prop,
-      // - or by looking up referenced resource with another field as criteria
-      
-      /*
-      // NB. single Italia country has to be filled at "install" time
-      company.set("!plo:country", UriHelper.buildUri(containerUrl, "!plo:country",
-            (String) company.get("!plo:country_name")));
-
-      company.set("!pli:city", UriHelper.buildUri(containerUrl, "!pli:city",
-            (String) company.get("!plo:country_name") + '/' + (String) company.get("!pli:city_name")));
-
-      // NB. ateco Model has to be filled at "install" time else code is not known
-      List<DCEntity> atecos = ldpEntityQueryService.findDataInType(modelAdminService.getModel("!coita:ateco"), new HashMap<String,List<String>>() {{
-               put("!?coita:atecoDescription", new ArrayList<String>() {{ add((String) company.get("!?coita:atecoDescription")); }}); }}, 0, 1);
-      DCResource ateco;
-      if (atecos != null && !atecos.isEmpty()) {
-         ateco = resourceEntityMapperService.entityToResource(atecos.get(0));
-      } else {
-         ///throw new RuntimeException("Unknown ateco description " + company.get("!?coita:atecoDescription"));
-         // WORKAROUND TO WRONG DESCRIPTIONS : (!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
-         // filling company's provided props :
-         ateco = DCResource.create(null, "!coita:ateco")
-               .set("!coita:atecoCode", ((String) company.get("!?coita:atecoDescription")).replace(' ', '_'))
-               .set("!?coita:atecoDescription", company.get("!?coita:atecoDescription"));
-         // once props are complete, build URI out of them and post :
-         ateco.setUriFromId(containerUrl, (String) ateco.get("!coita:atecoCode"));
-         /datacoreApiClient./postDataInType(ateco);
-         //resourcesToPost.add(ateco); // post and NOT schedule else can't be found in loop
-      }
-      company.set("!coita:atecoCode", ateco.get("!coita:atecoCode"));
-      company.set("!coita:ateco", ateco.getUri());
-      */
-
-      
-      // filling other Models that this table is a source of :
-      // TODO mixins ; fields ??
-      /*
-      try {
-         resourceService.get((String) company.get("!pli:city"), "!pli:city");
-      } catch (ResourceNotFoundException rnfex) {
-         /if (Response.Status.NOT_FOUND.getStatusCode() != waex.getResponse().getStatus()) {
-            throw new RuntimeException("Unexpected error", waex.getResponse().getEntity());
-         }/
-         DCResource city = DCResource.create((String) company.get("!pli:city"))
-               .set("!pli:city_name", company.get("!pli:city_name"))
-               .set("!plo:country_name", (String) company.get("!plo:country_name"))
-               .set("!plo:country", (String) company.get("!plo:country"));
-         // once props are complete, build URI out of them and (schedule) post :
-         ///city.setUriFromId(containerUrl, (String) company.get("!plo:country_name") + '/' + (String) company.get("!pli:city_name"));
-         /datacoreApiClient./postDataInType(city);
-         //resourcesToPost.add(city); // BEWARE must be posted before company else resource reference check fails
-      }
-      */
-   }
-   
-   
-   /** public for tests 
-    * @throws ResourceParsingException */
-   // TODO move to ModelResourceMappingService & modelAdminService.addModel() !
-   public ImmutableList<Object> fieldsToProps(Map<String, DCField> mapFields,
-         String fieldUriPrefix, List<Map<String, Object>> existingFieldsProps) throws ResourceParsingException {
-      ImmutableList.Builder<Object> fieldsPropBuilder = DCResource.listBuilder();
-      for (String fieldName : mapFields.keySet()) { // NB. ordered
-         DCField field = mapFields.get(fieldName);
-         Map<String,Object> existingFieldProps = (existingFieldsProps != null)
-               ? findFieldProps(fieldName, existingFieldsProps) : null;
-         fieldsPropBuilder.add(fieldToProps(field, fieldUriPrefix, existingFieldProps));
-      }
-      return fieldsPropBuilder.build();
-   }
-   
-   private Map<String,Object> findFieldProps(String fieldName,
-         List<Map<String, Object>> existingFieldsProps) {
-      for (Map<String, Object> existingFieldProps : existingFieldsProps) {
-         if (fieldName.equals(existingFieldProps.get("dcmf:name"))) {
-            return existingFieldProps;
-         }
-      }
-      return null;
-   }
-
-   /** public for tests 
-    * @param existingProps 
-    * @throws ResourceParsingException */
-   // TODO move to ModelResourceMappingService & modelAdminService.addModel() !
-   public /*Immutable*/Map<String, Object> fieldToProps(DCField field,
-         String fieldUriPrefix, Map<String,Object> existingProps) throws ResourceParsingException {
-      // building (dummy ?) field uri :
-      //String uri = aboveUri + '/' + field.getName();
-      // NO TODO pb uri must be of its own type and not its container's
-      String fieldUri = fieldUriPrefix + '/' + field.getName();
-      // TODO also for lists : /i
-      //ImmutableMap.Builder<String, Object> fieldPropBuilder = DCResource.propertiesBuilder();
-      DCResource.MapBuilder<String, Object> fieldPropBuilder;
-      if (existingProps != null) {
-         fieldPropBuilder = new DCResource.MapBuilder<String,Object>(existingProps);
-      } else {
-         fieldPropBuilder = new DCResource.MapBuilder<String,Object>();
-      }
-      fieldPropBuilder
-            //.put("@id", field.getName().replaceAll(":", "__")
-            //      .replaceAll("[!?]", "")) // relative (?), TODO TODO better solution for '!' start
-            .put("@id", fieldUri)
-            .put("@type", DCResource.listBuilder().add("dcmf:field_0").build()) // OR default one ?
-            .put("o:version", 0l) // dummy (OR should be top level's ??)
-            .put("dcmf:name", field.getName())
-            .put("dcmf:type", field.getType())
-            .put("dcmf:required", field.isRequired()) // TODO not for list ; for map ?!?
-            .put("dcmf:queryLimit", field.getQueryLimit()); // TODO not for list, map
-      String defaultStringValue = valueService.valueToString(field.getDefaultValue()); // TODO or not for list & map ?!?
-      if (defaultStringValue != null) {
-         fieldPropBuilder.put("dcmf:defaultStringValue", defaultStringValue);
-      } // else null means no value, but ImmutableMap.Builder doesn't accept null
-      switch(field.getType()) {
-      case "map" :
-         // NB. including DCI18nField's submap
-         DCMapField mapField = (DCMapField) field;
-         @SuppressWarnings("unchecked")
-         List<Map<String,Object>> existingMapFieldProps = (existingProps != null)
-               ? (List<Map<String,Object>>) existingProps.get("dcmf:mapFields") : null;
-         fieldPropBuilder.put("dcmf:mapFields",
-               fieldsToProps(mapField.getMapFields(), fieldUri, existingMapFieldProps));
-         break;
-      case "list" :
-      case "i18n" :
-         @SuppressWarnings("unchecked")
-         Map<String,Object> existingListElementFieldProps = (existingProps != null)
-            ? (Map<String,Object>) existingProps.get("dcmf:listElementField") : null;
-         fieldPropBuilder.put("dcmf:listElementField", fieldToProps(
-               ((DCListField) field).getListElementField(), fieldUri, existingListElementFieldProps));
-         break;
-      case "resource" :
-         fieldPropBuilder.put("dcmf:resourceType", ((DCResourceField) field).getResourceType());
-         // TODO LATER OPT also embedded Resource as map
-         break;
-      }
-      return fieldPropBuilder.build();
    }
    
 }
