493eb9c7a14c12eb9ba257c694732332383acb2e
Marc Dutoo <marc.dutoo@openwide.fr>
2015-11-10 17:20:57 +0100

    left-anchored regex, query optimization)

--- oasis-datacore-web/src/main/webapp/dc-ui/import/lib/datacore-import.js
+++ oasis-datacore-web/src/main/webapp/dc-ui/import/lib/datacore-import.js
@@ -2231,6 +2231,7 @@
       mergeStringValueOrDefaultIfAny(field, "dcmf:required", fieldRow["required"], importState.metamodel["dcmf:field_0"], importState);
       mergeStringValueOrDefaultIfAny(field, "dcmf:aliasedStorageNames", fieldRow["aliasedStorageNames"], importState.metamodel["dcmf:field_0"], importState);
       mergeStringValueOrDefaultIfAny(field, "dcmf:readonly", fieldRow["readonly"], importState.metamodel["dcmf:field_0"], importState);
+      mergeStringValueOrDefaultIfAny(field, "dcmf:fulltext", fieldRow["fulltext"], importState.metamodel["dcmf:field_0"], importState);
       mergeStringValueOrDefaultIfAny(field, "dcmf:queryLimit", fieldRow["queryLimit"],
             importState.metamodel["dcmf:field_0"], importState); // meaningless for list but required by ModelResourceMappingService for now
       mergeStringValueOrDefaultIfAny(field, "dcmf:isInMixinRef", fieldRow["isInMixinRef"],
@@ -2709,7 +2710,7 @@
             
          } else if (data._raw.statusCode === 403
                && skipForbidden(origResource, importState.model.posted, importState)) { // skip model
-            console.log("refreshAndSchedulePost skipping ", origResource['@id'] || origResource[0]['@id']);//
+            ///console.log("refreshAndSchedulePost skipping ", origResource['@id'] || origResource[0]['@id']);//
             postedCallback(null, origResource);
             
          } else {
