b64b6e35917008961998e49fb3fadfd4786f5fd6
Marc DUTOO <marc.dutoo@smile.fr>
2017-11-22 18:39:27 +0100

    added alerting for horizontal scalability using a Dropwizard Metrics custom Reporter that alerts (creates a file) if requests per minute is outside OCCI server- or property- configured min/max range.

--- ozwillo-datacore-rest-server/src/main/java/org/oasis/datacore/server/metrics/cxf/DatacoreCodahaleMetricsProvider.java
+++ ozwillo-datacore-rest-server/src/main/java/org/oasis/datacore/server/metrics/cxf/DatacoreCodahaleMetricsProvider.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.cxf.Bus;
 import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.metrics.MetricsContext;
@@ -21,14 +20,18 @@ import com.codahale.metrics.MetricRegistry;
 
 
 /**
- * Provides CXF codahale-based metrics, but with a CSV Reporter
- * with per minute rates to allow to compute requirements (D2.6 T5, see #48) :
+ * Setup of CXF codahale-based metrics, but with
+ * - a CSV reporter outputting "the maximum “number of requests per minute” per day" as required in specs
+ * - a custom reporter using the same metric to allow for horizontal scalability.
+ * 
+ * The CSV Reporter is conf'd with per minute rates to allow to compute requirements (D2.6 T5, see #48) :
  * "the maximum “number of requests per minute” per day",
  * which is then the daily max of the m1_rate (14th) column of the file
  * DatacoreApiImpl_Attribute=Totals.csv (or per operation).
  * 
  * Said one minute rate can be displayed in test by :
  * tail -f "target/metrics/DatacoreApiImpl_Attribute=Totals.csv" | awk ' { print $14 }' FS=","
+ * 
  * =>
  * 0.095510
  * 0.087874
@@ -36,11 +39,29 @@ import com.codahale.metrics.MetricRegistry;
  * 3.682107
  * 3.387702
  * 
+ * The custom scalability reporter uses CXF codahale-based metrics to provide horizontal scalability,
+ * by scheduling a reporter which triggers an alert if the currently
+ * measured number of requests per minute is outside defined min & max range.
+ * This min & max range is dynamically retrieved the configured OCCI server's configurations
+ * if any, or else from configured properties. Default is 0, which disables it.
+ * Said alert consists in creating a file on the file system, which can be detected by
+ * deployment & monitoring solutions such as Roboconf.
+ * 
+ * NB. both reporters can't be setup outside the single class that contains
+ * the MetricsRegistry (else registries beyond the first one contain no
+ * metrics, or accessing it from outside fails to resolve in Spring injection).
+ * 
+ * NB. CXF's Dropwizard metrics are only registered when the first request
+ * (to an endpoint...) is intercepted. Init only registers interceptors.
+ * 
  * @author mdutoo
  *
  */
 @Component("datacoreApiServer.metrics.provider")
 public class DatacoreCodahaleMetricsProvider extends CodahaleMetricsProvider {
+   
+   ////////////////////
+   // CSV
    
    /** metrics/ , below target/ for tests */
    //@Value("${datacoreApiServer.metrics.csvReportPath}")
@@ -49,6 +70,36 @@ public class DatacoreCodahaleMetricsProvider extends CodahaleMetricsProvider {
    //@Value("${datacoreApiServer.metrics.csvReportPeriod}")
    private long csvReportPeriod = 5;
 
+   ////////////////////
+   // SCALABILITY
+
+   /** 60 SECONDS (or finer ex. 5 for tests) */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.period}")
+   private long meanRequestThresholdPeriod = 5;
+   /** OCCI conf in (MART) server where the threshold is retrieved from, ex.
+    * http://localhost:8080/LDServicePerformanceSLOMixin/ or to test it .../compute/
+    * (if empty, explicit max/min props are used) */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.occiUrl}")
+   private String meanRequestThresholdOcciUrl = "";
+   /** ex. "/0/attributes/requestPerMinuteMax" or to test it "/0/attributes/occi.compute.cores" */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.maxOcciJsonPointer}")
+   private String meanRequestThresholdMaxOcciJsonPointer = "";
+   /** max threshold (only if no occiMartServerUrl, disabled if 0) */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.defaultMax}")
+   private float meanRequestThresholdMax = 0;
+   /** this file will be created by BrokenThresholdReporter if there is a max threshold and it is broken */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.maxAlertFilePath}")
+   private String meanRequestThresholdMaxAlertFilePath = "/tmp/vmfile";
+   /** ex. "/0/attributes/requestPerMinuteMin" */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.minOcciJsonPointer}")
+   private String meanRequestThresholdMinOcciJsonPointer = "";
+   /** min threshold (only if no occiMartServerUrl, disabled if 0) */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.defaultMin}")
+   private float meanRequestThresholdMin = 0;
+   /** this file will be created by BrokenThresholdReporter if there is a min threshold and it is broken */
+   //@Value("${datacoreApiServer.metrics.meanRequestThreshold.minAlertFilePath}")
+   private String meanRequestThresholdMinAlertFilePath = "/tmp/vmfilemin";
+   
    /**
     * NB. can't inject props as fields
     * 
@@ -58,19 +109,39 @@ public class DatacoreCodahaleMetricsProvider extends CodahaleMetricsProvider {
     * @param csvReportPeriod 60 SECONDS (or finer ex. 5 for tests)
     */
    @Autowired
-   public DatacoreCodahaleMetricsProvider(@Qualifier("datacoreApiServer") final Object datacoreApiServer,
+   public DatacoreCodahaleMetricsProvider(
+         @Qualifier("datacoreApiServer") final Object datacoreApiServer,
          @Value("${datacoreApiServer.metrics.csvReportPath}") final String csvReportPath,
-         @Value("${datacoreApiServer.metrics.csvReportPeriod}") final long csvReportPeriod) {
+         @Value("${datacoreApiServer.metrics.csvReportPeriod}") final long csvReportPeriod,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.period}") final long meanRequestThresholdPeriod,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.occiUrl}") final String meanRequestThresholdOcciUrl,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.maxOcciJsonPointer}") final String meanRequestThresholdMaxOcciJsonPointer,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.defaultMax}") final float meanRequestThresholdMax,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.maxAlertFilePath}") final String meanRequestThresholdMaxAlertFilePath,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.minOcciJsonPointer}") final String meanRequestThresholdMinOcciJsonPointer,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.defaultMin}") final float meanRequestThresholdMin,
+         @Value("${datacoreApiServer.metrics.meanRequestThreshold.minAlertFilePath}") final String meanRequestThresholdMinAlertFilePath) {
       super(((JAXRSServerFactoryBean) datacoreApiServer).getBus()); // NB. by default InstrumentationManager.class extension is missing
       // so won't create any JMX reporter
       
+      // csv
       this.csvReportPath = csvReportPath;
       this.csvReportPeriod = csvReportPeriod;
-      Bus b = ((JAXRSServerFactoryBean) datacoreApiServer).getBus();
-      setupCSVReporter(b, registry);
+      setupCSVReporter(this.registry);
+      
+      // scalability
+      this.meanRequestThresholdPeriod = meanRequestThresholdPeriod;
+      this.meanRequestThresholdOcciUrl = meanRequestThresholdOcciUrl;
+      this.meanRequestThresholdMaxOcciJsonPointer = meanRequestThresholdMaxOcciJsonPointer;
+      this.meanRequestThresholdMax = meanRequestThresholdMax;
+      this.meanRequestThresholdMaxAlertFilePath = meanRequestThresholdMaxAlertFilePath;
+      this.meanRequestThresholdMinOcciJsonPointer = meanRequestThresholdMinOcciJsonPointer;
+      this.meanRequestThresholdMin = meanRequestThresholdMin;
+      this.meanRequestThresholdMinAlertFilePath = meanRequestThresholdMinAlertFilePath;
+      setupMeanRequestThresholdAlertReporter(this.registry);
    }
 
-   private void setupCSVReporter(Bus b, MetricRegistry registry) {
+   private void setupCSVReporter(MetricRegistry registry) {
       if (csvReportPeriod <= 0) {
          return; // disabled
       }
@@ -89,6 +160,29 @@ public class DatacoreCodahaleMetricsProvider extends CodahaleMetricsProvider {
       reporter.start(csvReportPeriod, TimeUnit.SECONDS); // 60 SECONDS (or finer ex. for tests)
    }
 
+   private void setupMeanRequestThresholdAlertReporter(MetricRegistry registry) {
+      if (meanRequestThresholdPeriod <= 0) {
+         return; // disabled
+      }
+      
+      // (inspired by CXF')
+      if (meanRequestThresholdMax > 0 || meanRequestThresholdMin > 0) {
+         final MeanRequestThresholdAlertFileReporter meanThresholdBrokenFileReporter = MeanRequestThresholdAlertFileReporter.forRegistry(registry)
+               .convertRatesTo(TimeUnit.MINUTES) // MINUTES as per requirements
+               .convertDurationsTo(TimeUnit.MILLISECONDS)
+               .occiUrl(meanRequestThresholdOcciUrl)
+               .maxOcciJsonPointer(meanRequestThresholdMaxOcciJsonPointer)
+               .max(meanRequestThresholdMax)
+               .maxAlertFilePath(meanRequestThresholdMaxAlertFilePath)
+               .minOcciJsonPointer(meanRequestThresholdMinOcciJsonPointer)
+               .min(meanRequestThresholdMin)
+               .minAlertFilePath(meanRequestThresholdMinAlertFilePath)
+               .build();
+         meanThresholdBrokenFileReporter.start(meanRequestThresholdPeriod, TimeUnit.SECONDS); // 60 SECONDS (or finer ex. for tests)
+         // TODO LATER get csvReportPeriod from OCCI server
+      } // else disabled
+   }
+   
 
    // NOT override
    StringBuilder getBaseServiceName(Endpoint endpoint, boolean isClient, String clientId) {
---
+++ ozwillo-datacore-rest-server/src/main/java/org/oasis/datacore/server/metrics/cxf/MeanRequestThresholdAlertFileReporter.java
@@ -0,0 +1,329 @@
+package org.oasis.datacore.server.metrics.cxf;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.util.Locale;
+import java.util.SortedMap;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.Clock;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.ScheduledReporter;
+import com.codahale.metrics.Timer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * A reporter which allows for horizontal scalability, by triggering an alert if the currently
+ * measured number of requests per minute is outside defined min & max range.
+ * This min & max range is dynamically retrieved the configured OCCI server's configurations
+ * if any, or else from configured properties. Default is 0, which disables it.
+ * Said alert consists in creating a file on the file system, which can be detected by
+ * deployment & monitoring solutions such as Roboconf.
+ * 
+ * @author mdutoo
+ */
+public class MeanRequestThresholdAlertFileReporter extends ScheduledReporter {
+
+    private static final Logger logger = LoggerFactory.getLogger(MeanRequestThresholdAlertFileReporter.class);
+   
+    /**
+     * Returns a new {@link Builder} for {@link MeanRequestThresholdAlertFileReporter}.
+     *
+     * @param registry the registry to report
+     * @return a {@link Builder} instance for a {@link MeanRequestThresholdAlertFileReporter}
+     */
+    public static Builder forRegistry(MetricRegistry registry) {
+        return new Builder(registry);
+    }
+
+    /**
+     * A builder for {@link MeanRequestThresholdAlertFileReporter} instances.
+     */
+    public static class Builder {
+        private final MetricRegistry registry;
+        private Locale locale;
+        private Clock clock;
+        private TimeZone timeZone;
+        private TimeUnit rateUnit;
+        private TimeUnit durationUnit;
+        private MetricFilter filter;
+
+        /** OCCI conf in (MART) server where the threshold is retrieved from, ex.
+         * http://localhost:8080/LDServicePerformanceSLOMixin/ or to test it .../compute/
+         * (if empty, explicit max/min props are used) */
+        private String occiUrl = null;
+        /** ex. "/0/attributes/requestPerMinuteMax" or to test it "/0/attributes/occi.compute.cores" */
+        private String maxOcciJsonPointer = "";
+        /** max threshold (only if no occiMartServerUrl, disabled if 0) */
+        private float max = 0;
+        /** this file will be created by BrokenThresholdReporter if there is a max threshold and it is broken */
+        private String maxAlertFilePath = "/tmp/vmfile";
+        /** ex. "/0/attributes/requestPerMinuteMax" */
+        private String minOcciJsonPointer = "";
+        /** min threshold (only if no occiMartServerUrl, disabled if 0) */
+        private float min = 0;
+        /** this file will be created by BrokenThresholdReporter if there is a min threshold and it is broken */
+        private String minAlertFilePath = "/tmp/vmfilemin";
+
+        private Builder(MetricRegistry registry) {
+            this.registry = registry;
+            this.locale = Locale.getDefault();
+            this.clock = Clock.defaultClock();
+            this.timeZone = TimeZone.getDefault();
+            this.rateUnit = TimeUnit.SECONDS;
+            this.durationUnit = TimeUnit.MILLISECONDS;
+            this.filter = MetricFilter.ALL;
+        }
+
+        /**
+         * Format numbers for the given {@link Locale}.
+         *
+         * @param locale a {@link Locale}
+         * @return {@code this}
+         */
+        public Builder formattedFor(Locale locale) {
+            this.locale = locale;
+            return this;
+        }
+
+        /**
+         * Use the given {@link Clock} instance for the time.
+         *
+         * @param clock a {@link Clock} instance
+         * @return {@code this}
+         */
+        public Builder withClock(Clock clock) {
+            this.clock = clock;
+            return this;
+        }
+
+        /**
+         * Use the given {@link TimeZone} for the time.
+         *
+         * @param timeZone a {@link TimeZone}
+         * @return {@code this}
+         */
+        public Builder formattedFor(TimeZone timeZone) {
+            this.timeZone = timeZone;
+            return this;
+        }
+
+        /**
+         * Convert rates to the given time unit.
+         *
+         * @param rateUnit a unit of time
+         * @return {@code this}
+         */
+        public Builder convertRatesTo(TimeUnit rateUnit) {
+            this.rateUnit = rateUnit;
+            return this;
+        }
+
+        /**
+         * Convert durations to the given time unit.
+         *
+         * @param durationUnit a unit of time
+         * @return {@code this}
+         */
+        public Builder convertDurationsTo(TimeUnit durationUnit) {
+            this.durationUnit = durationUnit;
+            return this;
+        }
+
+        /**
+         * Only report metrics which match the given filter.
+         *
+         * @param filter a {@link MetricFilter}
+         * @return {@code this}
+         */
+        public Builder filter(MetricFilter filter) {
+            this.filter = filter;
+            return this;
+        }
+
+        public Builder occiUrl(String occiUrl) {
+           this.occiUrl = occiUrl;
+           return this;
+        }
+        public Builder maxOcciJsonPointer(String maxOcciJsonPointer) {
+           this.maxOcciJsonPointer = maxOcciJsonPointer;
+           return this;
+        }
+        public Builder max(float max) {
+           this.max = max;
+           return this;
+        }
+        public Builder maxAlertFilePath(String maxAlertFilePath) {
+           this.maxAlertFilePath = maxAlertFilePath;
+           return this;
+        }
+        public Builder minOcciJsonPointer(String minOcciJsonPointer) {
+           this.minOcciJsonPointer = minOcciJsonPointer;
+           return this;
+        }
+        public Builder min(float min) {
+           this.min = min;
+           return this;
+        }
+        public Builder minAlertFilePath(String minAlertFilePath) {
+           this.minAlertFilePath = minAlertFilePath;
+           return this;
+        }
+
+        /**
+         * Builds a {@link MeanRequestThresholdAlertFileReporter} with the given properties.
+         *
+         * @return a {@link MeanRequestThresholdAlertFileReporter}
+         */
+        public MeanRequestThresholdAlertFileReporter build() {
+            return new MeanRequestThresholdAlertFileReporter(registry,
+                                       locale,
+                                       clock,
+                                       timeZone,
+                                       rateUnit,
+                                       durationUnit,
+                                       filter,
+                                       occiUrl,
+                                       maxOcciJsonPointer,
+                                       max,
+                                       maxAlertFilePath,
+                                       minOcciJsonPointer,
+                                       min,
+                                       minAlertFilePath);
+        }
+    }
+
+    private final DateFormat dateFormat;
+
+    /** OCCI conf in (MART) server where the threshold is retrieved from, ex.
+     * http://localhost:8080/LDServicePerformanceSLOMixin/ or to test it .../compute/
+     * (if empty, explicit max/min props are used) */
+    private URL occiUrl = null;
+    /** ex. "/0/attributes/requestPerMinuteMax" or to test it "/0/attributes/occi.compute.cores" */
+    private String maxOcciJsonPointer = "";
+    /** max threshold (disabled if 0) */
+    private float max = 0;
+    /** this file will be created by BrokenThresholdReporter if there is a max threshold and it is broken */
+    private String maxAlertFilePath = "/tmp/vmfile";
+    /** ex. "/0/attributes/requestPerMinuteMin" */
+    private String minOcciJsonPointer = "";
+    /** min threshold (disabled if 0) */
+    private float min = 0;
+    /** this file will be created by BrokenThresholdReporter if there is a min threshold and it is broken */
+    private String minAlertFilePath = "/tmp/vmfilemin";
+    private String timerName = "DatacoreApiImpl_Attribute=Totals";
+    private ObjectMapper mapper = new ObjectMapper(); // to parse OCCI
+
+    private MeanRequestThresholdAlertFileReporter(MetricRegistry registry,
+                            Locale locale,
+                            Clock clock,
+                            TimeZone timeZone,
+                            TimeUnit rateUnit,
+                            TimeUnit durationUnit,
+                            MetricFilter filter,
+                            String occiUrl,
+                            String maxOcciJsonPointer,
+                            float max,
+                            String maxAlertFilePath,
+                            String minOcciJsonPointer,
+                            float min,
+                            String minAlertFilePath) {
+        super(registry, "MeanRequestThresholdAlertFile-reporter", filter, rateUnit, durationUnit);
+        this.dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT,
+                                                         DateFormat.MEDIUM,
+                                                         locale);
+        dateFormat.setTimeZone(timeZone);
+        
+        this.maxOcciJsonPointer = maxOcciJsonPointer;
+        this.max = max;
+        this.maxAlertFilePath = maxAlertFilePath;
+        this.minOcciJsonPointer = minOcciJsonPointer;
+        this.min = min;
+        this.minAlertFilePath = minAlertFilePath;
+        
+        if (occiUrl != null && !occiUrl.isEmpty()) {
+           try {
+              this.occiUrl = new URL(occiUrl);
+           } catch (MalformedURLException e) {
+              logger.error("Bad URL for datacoreApiServer.metrics.meanRequestThreshold.occiUrl : " + occiUrl, e);
+           }
+        }
+    }
+
+    @Override
+    public void report(SortedMap<String, Gauge> gauges,
+                       SortedMap<String, Counter> counters,
+                       SortedMap<String, Histogram> histograms,
+                       SortedMap<String, Meter> meters,
+                       SortedMap<String, Timer> timers) {
+       // get value :
+       Timer datacoreApiTimer = timers.get(timerName);
+       if (datacoreApiTimer == null) {
+          return; // no request yet, not not metrics have been initialized
+       }
+       double oneMinuteRate = convertRate(datacoreApiTimer.getOneMinuteRate());
+       
+       // get thresholds :
+       float max = this.max;
+       float min = this.min;
+       if (this.occiUrl != null) {
+          try {
+             // get SLO conf from OCCI server :
+             URLConnection httpConn = this.occiUrl.openConnection();
+             httpConn.setRequestProperty("Accept", "application/json");
+             httpConn.connect();
+             String occiJsonRes = IOUtils.toString(httpConn.getInputStream());
+             JsonNode occiJsonNode = mapper.readTree(occiJsonRes); 
+             max = (float) occiJsonNode.at(this.maxOcciJsonPointer).asDouble(0);
+             min = (float) occiJsonNode.at(this.minOcciJsonPointer).asDouble(0);
+         } catch (Exception e) {
+            logger.error("Error retrieving min/max thresholds from OCCI MARTServer "
+                  + this.occiUrl + " using JSON pointer expressions "
+                  + this.minOcciJsonPointer + " "
+                  + this.maxOcciJsonPointer);
+         }
+       }
+       
+       // compare :
+       File alertFile = null;
+       if (oneMinuteRate > max) {
+          alertFile = new File(this.maxAlertFilePath);
+       } else if (oneMinuteRate < min) {
+          alertFile = new File(this.minAlertFilePath);
+       }
+
+       // act :
+       if (alertFile != null) {
+          String msg = String.format("[min=%f, max=%f, current=%f]: ",
+                min, max, oneMinuteRate);
+          if (alertFile.exists()) {
+             logger.error(msg + "alert file already exists, deleting " + alertFile.getAbsolutePath());
+             alertFile.delete();
+          } else {
+             logger.warn(msg + "creating max alert file " + alertFile.getAbsolutePath());
+             try {
+                alertFile.createNewFile();
+             } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+             }
+          }
+       }
+    }
+    
+}
